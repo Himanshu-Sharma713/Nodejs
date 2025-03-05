6a5610420ec5324ce71f0710320ae7e15053ec48
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-03-05 18:40:07 +0530

    create file using nodejs

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // first NodeJS Code
 
 const fs = require("node:fs/promises");
+
+let filepath = "input.txt";
+const message = "I learn NodeJS, ExpressJS and MongoDB..............";
+
+fs.writeFile(filepath, message, (err) => {
+  if (err) console.error("Error: ", err);
+  else console.log("Created file SuccessFully...");
+});
