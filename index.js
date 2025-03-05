4ffa20c8f5bf34897b9a47b31a2528a60b212482
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-03-05 19:33:29 +0530

    file system

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 // first NodeJS Code
 
-const fs = require("node:fs/promises");
+const fs = require("node:fs");
 
 let filepath = "input.txt";
-const message = "I learn NodeJS, ExpressJS and MongoDB..............";
+const message = "I learn NodeJS, ExpressJS and MongoDB..............\n";
+const updateMessage = "Himanshu Shamra\n";
+
+// Write File in Node JS...
 
 fs.writeFile(filepath, message, (err) => {
-  if (err) console.error("Error: ", err);
-  else console.log("Created file SuccessFully...");
+  if (err) console.error(err);
+  else console.log("Created File Successfully...");
 });
+
+// update File in Node JS
+
+// fs.appendFile(filepath, updateMessage, (err) => {
+//   if (err) console.error(err);
+//   else console.log("Updated File Successfully...");
+// });
+
+// read file data in Node JS
+
+// fs.readFile(filepath, "utf8", (err, data) => {
+//   if (err) console.error(err);
+//   else console.log("Data: ", data);
+// });
+
+// Delete file in Node JS
+
+// fs.unlink(filepath, (err) => {
+//   if (err) console.error(err);
+//   else console.log("Deleted File Successfully...");
+// });
